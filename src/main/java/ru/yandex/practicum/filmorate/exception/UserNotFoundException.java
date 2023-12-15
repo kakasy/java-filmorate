b74c54b22a38590712b9df96b9759a0369879d93
kakasy <drum2to@yandex.ru>
2023-12-15 22:20:25 +0300

    feat: добавить исключения для обработки ошибок при работе с фильмами и пользователями

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UserNotFoundException extends IllegalArgumentException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+        log.error(message);
+    }
+
+}
