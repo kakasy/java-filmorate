2b68946027ee2c1c591ad44869ca3cf73c5aa648
kakasy <drum2to@yandex.ru>
2023-12-17 03:56:01 +0300

    refactor: убрать логирование (вынесено в обработчик)

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -1,13 +1,9 @@
 package ru.yandex.practicum.filmorate.exception;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class FilmNotFoundException extends IllegalArgumentException {
 
     public FilmNotFoundException(String message) {
         super(message);
-        log.error(message);
     }
 
 }
