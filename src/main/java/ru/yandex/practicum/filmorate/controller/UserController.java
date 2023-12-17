0ce194079a2ec548ee803b0468f5fe63e5945acb
kakasy <drum2to@yandex.ru>
2023-12-17 04:00:36 +0300

    refactor:  добавить @RequiredArgsConstructor, убрать явное внедрение зависимостей через конструктор

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
@@ -11,15 +12,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-
-
-    public UserController(UserService userService) {
-
-        this.userService = userService;
-    }
 
     @GetMapping
     public List<User> getAll() {
