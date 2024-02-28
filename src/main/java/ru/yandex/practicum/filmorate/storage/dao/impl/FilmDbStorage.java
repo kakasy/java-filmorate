cf480e7e23bed591571ea664525516bc02dbeaab
kakasy <drum2to@yandex.ru>
2024-02-28 09:43:26 +0300

    refactor: вынести методы, работающие с жанрами в GenreStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/impl/FilmDbStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.dao.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
@@ -40,7 +39,6 @@ public class FilmDbStorage implements FilmStorage {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         String sqlQuery = "INSERT INTO films (name, description, release_date, duration, rating_id) " +
                 "VALUES (?, ?, ?, ?, ?);";
-        String queryForFilmGenre = "INSERT INTO films_genres (film_id, genre_id) VALUES (?, ?);";
 
         jdbcTemplate.update(connection -> {
             PreparedStatement stmt =
@@ -54,12 +52,6 @@ public class FilmDbStorage implements FilmStorage {
         }, keyHolder);
 
         film.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
-//
-//        if (!film.getGenres().isEmpty()) {
-//            List<Genre> genres =  new ArrayList<>(film.getGenres());
-//            updateBatchFilmGenres(queryForFilmGenre, film, genres);
-//        }
-
 
         return film;
     }
@@ -79,16 +71,11 @@ public class FilmDbStorage implements FilmStorage {
         String sqlQuery = "UPDATE films SET name=?, description=?, release_date=?, rating_id=?, duration=?" +
                 " WHERE film_id=?";
         String sqlQueryToDeleteGenres = "DELETE FROM films_genres WHERE film_id=?"; // в жанры
-        String sqlQueryUpdateGenres = "INSERT INTO films_genres (film_id, genre_id) VALUES (?, ?);";
 
         jdbcTemplate.update(sqlQuery, film.getName(), film.getDescription(), film.getReleaseDate(),
                 film.getMpa().getId(), film.getDuration(), film.getId());
 
         jdbcTemplate.update(sqlQueryToDeleteGenres, film.getId());
-//        if (!film.getGenres().isEmpty()) {
-//            List<Genre> genres = new ArrayList<>(film.getGenres());
-//            updateBatchFilmGenres(sqlQueryUpdateGenres, film, genres);
-//        }
 
         return film;
     }
@@ -110,9 +97,6 @@ public class FilmDbStorage implements FilmStorage {
                     .mpa(new Mpa(filmRows.getInt("rating_id"), filmRows.getString("rating_name")))
                     .build();
 
-//            List<Genre> genres = getFilmGenres(filmId);
-//
-//            film.getGenres().addAll(genres);
 
             log.info("Найден фильм с id {}", filmId);
             return Optional.of(film);
@@ -134,7 +118,7 @@ public class FilmDbStorage implements FilmStorage {
 
     private Film mapRowToFilm(ResultSet rs, int rowNum) throws SQLException {
 
-        Film film = Film.builder()
+        return Film.builder()
                 .id(rs.getLong("film_id"))
                 .name(rs.getString("name"))
                 .description(rs.getString("description"))
@@ -142,41 +126,6 @@ public class FilmDbStorage implements FilmStorage {
                 .duration(rs.getInt("duration"))
                 .mpa(new Mpa(rs.getInt("rating_id"), rs.getString("rating_name")))
                 .build();
-
-//        List<Genre> genres = getFilmGenres(film.getId());
-//
-//        film.getGenres().addAll(genres);
-
-        return film;
-    }
-
-//    private Genre mapRowToGenre(ResultSet rs, int rowNum) throws SQLException {
-//
-//        return new Genre(rs.getInt("genre_id"), rs.getString("name"));
-//    }
-
-//    private List<Genre> getFilmGenres(Long filmId) {
-//
-//        String queryForFilmGenres = "SELECT fg.film_id, fg.genre_id, g.name FROM films_genres AS fg" +
-//                " JOIN genres AS g ON g.genre_id = fg.genre_id WHERE film_id=?";
-//        return jdbcTemplate.query(queryForFilmGenres, this::mapRowToGenre, filmId);
-//    }
-
-    private void updateBatchFilmGenres(String sql, Film film, List<Genre> genres) {
-
-        jdbcTemplate.batchUpdate(sql,
-                new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setLong(1, film.getId());
-                        ps.setInt(2, genres.get(i).getId());
-                    }
-
-                    @Override
-                    public int getBatchSize() {
-                        return film.getGenres().size();
-                    }
-                });
     }
 }
 
